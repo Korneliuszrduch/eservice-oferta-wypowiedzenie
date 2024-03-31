20471abc17e1cbbb719515cb0afe8760aa0453a1
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-03-31 15:31:14 +0200

    add service buttoms

--- js/netsendo.js
+++ js/netsendo.js
@@ -30,3 +30,55 @@ fetch('https://mail.korneliuszrduch.pl/subscribe.php', {
 })
     .then(response => response.json())
     .then(response => console.log(JSON.stringify(response)))
+
+
+
+
+
+
+
+
+    <form accept-charset="UTF-8" action="https://mail.korneliuszrduch.pl/subscribe.php" method="POST"><div id="sqm-liveform" data-enhance="false" style="border-width: 10px 3px; border-radius: 5px; background: rgb(242, 242, 242); border-color: rgb(0, 0, 0); color: rgb(0, 0, 0); padding: 20px; border-style: solid;">  
+    <div class="sqm-description" style="margin-bottom: 20px; width: 100%; float: none; padding-right: 0px; display: none;" wfd-invisible="true">
+        przykładowy opis                                    </div>
+    <div class="sqm-form" style="float: none; width: 100%;">
+        <div class="sqm-fields" style="float: none;"><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-femail" style="width: inherit;">Adres e-mail</label><input type="text" name="email" class="form-control sqm-textinput" id="sqm-femail" value="" data-placeholder="Adres e-mail" placeholder="Adres e-mail" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f293" style="width: inherit;">Imię i nazwisko osoba do kontaktu w punkcie 1</label><input type="text" name="fields[293]" class="form-control sqm-textinput" id="sqm-f293" value="" data-placeholder="Imię i nazwisko osoba do kontaktu w punkcie 1" placeholder="Imię i nazwisko osoba do kontaktu w punkcie 1" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f294" style="width: inherit;">Telefon osoba do kontaktu w punkcie1</label><input type="text" name="fields[294]" class="form-control sqm-textinput" id="sqm-f294" value="" data-placeholder="Telefon osoba do kontaktu w punkcie1" placeholder="Telefon osoba do kontaktu w punkcie1" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f295" style="width: inherit;">Nazwa punktu terminal 2</label><input type="text" name="fields[295]" class="form-control sqm-textinput" id="sqm-f295" value="" data-placeholder="Nazwa punktu terminal 2" placeholder="Nazwa punktu terminal 2" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f296" style="width: inherit;">Nazwa ulicy punktu terminal 2</label><input type="text" name="fields[296]" class="form-control sqm-textinput" id="sqm-f296" value="" data-placeholder="Nazwa ulicy punktu terminal 2" placeholder="Nazwa ulicy punktu terminal 2" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f297" style="width: inherit;">Nazwa miasta punktu 2 terminal</label><input type="text" name="fields[297]" class="form-control sqm-textinput" id="sqm-f297" value="" data-placeholder="Nazwa miasta punktu 2 terminal" placeholder="Nazwa miasta punktu 2 terminal" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f298" style="width: inherit;">Kod pocztowy punktu terminal 2</label><input type="text" name="fields[298]" class="form-control sqm-textinput" id="sqm-f298" value="" data-placeholder="Kod pocztowy punktu terminal 2" placeholder="Kod pocztowy punktu terminal 2" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f299" style="width: inherit;">Imię i nazwisko osoby kontaktowej w punkcie 2</label><input type="text" name="fields[299]" class="form-control sqm-textinput" id="sqm-f299" value="" data-placeholder="Imię i nazwisko osoby kontaktowej w punkcie 2" placeholder="Imię i nazwisko osoby kontaktowej w punkcie 2" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f300" style="width: inherit;">Telefon osoba kontaktowa punkt 2</label><input type="text" name="fields[300]" class="form-control sqm-textinput" id="sqm-f300" value="" data-placeholder="Telefon osoba kontaktowa punkt 2" placeholder="Telefon osoba kontaktowa punkt 2" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f301" style="width: inherit;">Numer konta bankowego punktu 2</label><input type="text" name="fields[301]" class="form-control sqm-textinput" id="sqm-f301" value="" data-placeholder="Numer konta bankowego punktu 2" placeholder="Numer konta bankowego punktu 2" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f302" style="width: inherit;">Terminal stacjonarny punkt 2</label><select name="fields[302]" id="sqm-f302" class="form-control sqm-textinput" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "><option value=""></option></select></div><div class="sqm-form-group" style="margin: 10px 0px; display: block;"><label for="sqm-f303" style="width: inherit;">Terminal przenośny punkt 2</label><select name="fields[303]" id="sqm-f303" class="form-control sqm-textinput" style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 0); border-color: rgb(63, 81, 181); width: 100%; display: block;" onblur="this.style.backgroundColor = '#ffffff'; this.style.color = '#000000'; this.style.borderColor = '#3f51b5'; " onfocus="this.style.backgroundColor = '#d7d7d7'; this.style.color = '#3f51b5'; this.style.borderColor = '#ffffff'; "><option value=""></option></select></div></div>
+        <div id="sqm-coregister" style="display: none;" wfd-invisible="true">
+            <h5 id="sqm-coregister-text">Zapisz mnie również na poniższe listy:</h5>
+        </div>
+        <div id="sqm-submit-box" style="width: 100%; text-align: left; float: none;"><div id="sqm-submit1" class="sqm-form-group form-group" style="margin: 10px 0px; text-align: left; display: block;"><input type="submit" class="sqm-submit" id="sqm-submit" value="Zapisz się" style="border-style: solid; background: rgb(63, 81, 181); color: rgb(255, 255, 255); border-color: rgb(102, 181, 240); border-width: 0px 0px 4px; border-radius: 3px; padding: 7px 25px; margin: 0px; font-size: 16px;" onmouseover="this.style.backgroundColor = '#98c8eb';" onmouseout="this.style.backgroundColor = '#3f51b5';"></div>
+            
+        </div>
+    </div>
+    <div style="clear: both;"></div>
+</div><input type="hidden" name="mlid" value="86" />
+<input type="hidden" name="req" value="email" />
+<input type="hidden" name="token" value="29ee236865e35b38aa28bb36f16ab616992701be" />
+<input type="hidden" name="coregister" value="" />
+
+</form>
+
+
+
+
+nazwapunktuinstalacjiterminal2 295
+ulicainstalacjiterminal2 
+
+Nazwa miasta punktu 2 terminal [[miastoinstalacjiterminal2]] 297
+
+Kod pocztowy punktu terminal 2
+[[kodpocztowyinstalacjiterminal2]]
+Imię i nazwisko osoby kontaktowej  299
+[[osobakontaktowapunkt2]] 
+
+Telefon do osoby kontaktowej punkt 
+[[telefonosobadokontaktuwpunkcie2]] fields 300
+Numer konta bankowego punktu 2
+[[numerrachunkubankowegoterminalpunkt2]] firelds 301
+
+Terminal stacjonarny punkt 2
+[[terminalstacjonarnypunkt2]] fielsd 302
+Terminal przenośny punkt 2
+[[terminalprzenosnypunkt2]] fielsd 303
+
+[[numerbudynkuilokalupunkt2]] 304
---
+++ js/script_place.js
@@ -0,0 +1,292 @@
+
+/*
+ {
+    document.addEventListener("DOMContentLoaded", function () {
+
+        const handleRemoveButton = document.querySelector(".js-button--removePlace");
+        const addPlaceButton = document.querySelector(".js-button--addPlace");
+        const form = document.querySelector(".form--dataCompany");
+
+        let currentIndex = 0;
+
+
+        handleRemoveButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            const sections = document.querySelectorAll(".section--place");
+
+            if (sections.length > 1) {
+                const lastSection = sections[sections.length - 1];
+                lastSection.remove();
+                currentIndex = currentIndex > 0 ? currentIndex - 1 : 0;
+            }
+        });
+
+
+        addPlaceButton.addEventListener("click", function (event) {
+            event.preventDefault();
+            currentIndex++;
+            const newSection = document.createElement("section");
+            newSection.className = "section section--place";
+            newSection.setAttribute("data-index", currentIndex);
+
+            // Wykorzystujemy dane z odpowiedniego przypadku
+            switch (currentIndex) {
+                case 1:
+                    newSection.innerHTML = case1;
+                    break;
+                case 2:
+                    newSection.innerHTML = case2;
+                    break;
+                case 3:
+                    newSection.innerHTML = case3;
+                    break;
+                default:
+                    break;
+            }
+
+            // Dodajemy nową sekcję do formularza
+            form.appendChild(newSection);
+        });
+
+        const case1 = `
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f295">Nazwa punktu</label>
+                    <input class="input" id="sqm-f295" name="fields[295]" type="text"
+                        value="[[nazwapunktuinstalacjiterminal2]]" placeholder="Nazwa punktu 2"
+                        data-placeholder="Nazwa punktu" required />
+                </section>
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f296">Nazwa ulicy instalacji terminala</label><input class="input"
+                        id="sqm-f296" name="fields[296]" type="text" value="[[ulicainstalacjiterminal2]] "
+                        placeholder="Ulica instalacji terminala" data-placeholder="Ulica instalacji terminala"
+                        required />
+                </section>
+
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f304">Numer budynku / numer lokalu</label>
+                    <input class="input" id="sqm-f304" name="fields[304]" type="text"
+                        value="[[numerbudynkuilokalupunkt2]]" placeholder="Numer budynku"
+                        data-placeholder="Numer budynku / numer lokalu" required />
+                </section>
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f297">Miasto instalacji terminala</label>
+                    <input class="input" id="sqm-f297" name="fields[297]" type="text"
+                        value="[[miastoinstalacjiterminal2]]" placeholder="Miasto instalacji"
+                        data-placeholder="Miasto instalacji terminala" required />
+                </section>
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f298">Kod pocztowy (XX-XXX)</label>
+                    <input class="input" id="sqm-f298" name="fields[298]" type="text"
+                        value="[[kodpocztowyinstalacjiterminal2]]" placeholder="Kod pocztowy"
+                        data-placeholder="Kod pocztowy" required />
+                </section>
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f299">Imię i nazwisko osoby kontaktowej w punkcie</label>
+                    <input class="input" id="sqm-f299" name="fields[299]" type="text"
+                        value="[[osobakontaktowapunkt2]] " placeholder="Imię i nazwisko osoby kontaktowej"
+                        data-placeholder="Imię i nazwisko osoby kontaktowej" required />
+                </section>
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f300">Telefon kontaktowy w punkcie</label>
+                    <input class="input" id="sqm-f300" name="fields[300]" type="text"
+                        value="[[telefonosobadokontaktuwpunkcie2]]" placeholder="Telefon kontaktowy w punkcie"
+                        data-placeholder="Telefon kontaktowy w punkcie" required />
+                </section>
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f301">Numer rachunku bankowego</label>
+                    <input class="input" id="sqm-f301" name="fields[301]" type="text"
+                        value="[[numerrachunkubankowegoterminalpunkt2]]" placeholder="Numer rachunku bankowwego"
+                        data-placeholder="Numer rachunku bankowego" required />
+                </section>
+
+
+
+
+
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f302"> Terminal stacjonarny </label>
+                    <select class="select" id="sqm-f302" name="fields[302]" type="text" required>
+                        <option value="[[terminalstacjonarnypunkt2]]">[[terminalstacjonarnypunkt2]]
+                        </option>
+                        <option value="1 x Terminal stacjonarny">1 x Terminal stacjonarny</option>
+                        <option value="2 x Terminale stacjonarne">2 x Terminale stacjonarne</option>
+                        <option value="3 x Terminale stacjonarne">3 x Terminale stacjonarne</option>
+
+                        <option value="1 x Terminal stacjonarny z pinpadem">1 x Terminal stacjonarny z pinpadem
+                        </option>
+                        <option value="2 x Terminale stacjonarne z pinpadem">2 x Terminale stacjonarne z pinpadem
+                        </option>
+                        <option value="3 x Terminale stacjonarne z pinpadem">3 x Terminale stacjonarne z pinpadem
+                        </option>
+                        <option value="Liczba zgodna z ustaleniami">Liczba zgodna z ustaleniami</option>
+                        <option value="Nie dotyczy">Nie dotyczy</option>
+                        <option value=""></option>
+                    </select>
+                </section>
+
+                <section class="section section--dataCompany">
+                    <label class="label" for="sqm-f303"> Terminal przenośny </label>
+                    <select class="select" id="sqm-f303" name="fields[303]" type="text" required>
+
+                        <option value="[[terminalprzenosnypunkt2]]">[[terminalprzenosnypunkt2]] </option>
+                        <option value="1 x Terminal Przenośny">1 x Terminal przenośny</option>
+                        <option value="2 x Terminale Przenośne">2 x Terminale przenośne</option>
+                        <option value="3 x Terminale Przenośne">3 x Terminale przenośne</option>
+                        <option value="Aplikacja na telefon">Aplikacja na telefon</option>Przenośny</option>
+                        <option value="Liczba zgodna z ustaleniami">Liczba zgodna z ustaleniami</option>
+                        <option value="Nie dotyczy">Nie dotyczy</option>
+                        <option value=""></option>
+                    </select>
+                </section>
+
+           
+    `;
+
+        const case2 = `
+        <label class="label" for="sqm-fdane">Nazwa punktu 2</label>
+        <input class="input" id="sqm-fdane" name="fields[dane]" type="text" required
+            value="[[nazwapunktuinstalacjiterminal2]]" placeholder="Nazwa punktu 2" data-placeholder="Nazwa punktu szczecin" />
+        <!-- Dodaj pozostałe pola z case2 -->
+    `;
+
+        const case3 = `
+        <label class="label" for="sqm-fdate">Nazwa punktu szczecin</label>
+        <input class="input" id="sqm-fdate" name="fields[date]" type="text" required
+            value="[[nazwapunktuinstalacjiterminal3333]]" placeholder="Nazwa punktu 3" data-placeholder="Nazwa punktu bialystok" />
+        <!-- Dodaj pozostałe pola z case3 -->
+    `;
+
+
+        const handleButtonNextStep = document.querySelector(".js-button--nextStep");
+
+        const sendForm = (event) => {
+            event.preventDefault();
+
+            if (!validateCurrentSection()) {
+                alert("Wypełnij wszystkie wymagane pola przed przejściem dalej.");
+                return;
+            }
+
+            form.submit();
+        };
+
+        function validateCurrentSection() {
+            // Pobieramy wszystkie wymagane pola w bieżącej sekcji
+            const requiredFields = document.querySelectorAll(".section--place [required]");
+
+            // Sprawdzamy, czy wszystkie wymagane pola są wypełnione
+            const emptyFields = [];
+            const isSectionValid = Array.from(requiredFields).every(field => {
+                const isValid = field.value.trim() !== "";
+
+                // Jeśli pole jest puste, dodajmy klasę 'invalid' dla wizualnego oznaczenia
+                if (!isValid) {
+                    field.classList.add('invalid');
+                    emptyFields.push(field.id);
+                } else {
+                    field.classList.remove('invalid');
+                }
+
+                return isValid;
+            });
+
+            // Jeśli sekcja jest nieprawidłowa, wyświetlamy komunikat z pustymi polami
+            if (!isSectionValid) {
+                alert(`Wypełnij wszystkie wymagane pola przed przejściem dalej. Puste pola: `);
+            }
+
+            return isSectionValid;
+        }
+
+        // Dodanie funkcji obsługi zdarzenia kliknięcia przycisku "Dalej"
+        handleButtonNextStep.addEventListener("click", sendForm);
+    });
+
+
+
+
+    const handBackButton = document.querySelector(".js-button--backStep");
+    handBackButton.addEventListener("click", function () {
+        handformBackStep = document.querySelector(".formBackStep");
+        handformBackStep.submit();
+
+
+
+
+    });
+
+
+   
+
+       
+}
+
+*/
+
+
+
+{
+    document.addEventListener('DOMContentLoaded', () => {
+        const forms = document.querySelectorAll("form");
+        forms.forEach(form => {
+            form.addEventListener("submit", event => {
+                event.preventDefault(); // Zatrzymuje domyślną akcję formularza (czyli wysłanie danych)
+                // Tutaj możesz dodać kod, który wykonuje się po naciśnięciu przycisku "Dodaj punkt"
+            });
+        });
+
+        const sectionsPlace = document.querySelectorAll(".section--place");
+        let currentSectionIndex = 0;
+        for (let i = 1; i < sectionsPlace.length; i++) {
+            sectionsPlace[i].style.display = "none";
+        };
+        const formDataCompany = document.querySelector(".form--dataCompany");
+        const buttonNextStep = document.querySelector(".js-button--nextStep");
+        const formBackStep = document.querySelector(".formBackStep");
+        const buttonBackStep = document.querySelector(".js-button--backStep");
+        const addPlaceButton = document.querySelector(".js-button--addPlace");
+        const removePlaceButtton = document.querySelector(".js-button--removePlace");
+
+        buttonNextStep.addEventListener("click", () => sendFormDataCompany());
+
+
+        const sendFormDataCompany = () => {
+            formDataCompany.submit();
+        };
+
+        buttonBackStep.addEventListener("click", () => sendFormBackStep());
+        const sendFormBackStep = () => {
+            formBackStep.submit();
+        };
+
+        addPlaceButton.addEventListener("click", () => showHiddenPlace());
+
+        const showHiddenPlace = () => {
+            if (currentSectionIndex < sectionsPlace.length - 1) {
+               currentSectionIndex++;
+                sectionsPlace[currentSectionIndex].style.display = "block";
+                console.log("dodalem point" ,currentSectionIndex);
+            };
+        };
+    
+
+        removePlaceButtton.addEventListener("click", () => {
+           if(currentSectionIndex > 0) {
+            sectionsPlace[currentSectionIndex].style.display ="none"
+            currentSectionIndex--;
+            console.log("usunąłem Punkt", currentSectionIndex);
+           };
+
+
+        });
+
+    });
+}
