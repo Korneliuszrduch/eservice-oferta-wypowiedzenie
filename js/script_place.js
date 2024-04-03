c4c7b3d26069d7b47c3938ce254ba95b94d3e2b2
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-04-03 15:57:50 +0200

    fix send input

--- js/script_place.js
+++ js/script_place.js
@@ -242,11 +242,12 @@
                 // Tutaj możesz dodać kod, który wykonuje się po naciśnięciu przycisku "Dodaj punkt"
             });
         });
-
+    
         const sectionsPlace = document.querySelectorAll(".section--place");
         let currentSectionIndex = 0;
         for (let i = 1; i < sectionsPlace.length; i++) {
             sectionsPlace[i].style.display = "none";
+            sectionsPlace[i].classList.add("hidden");
         };
         const formDataCompany = document.querySelector(".form--dataCompany");
         const buttonNextStep = document.querySelector(".js-button--nextStep");
@@ -254,39 +255,83 @@
         const buttonBackStep = document.querySelector(".js-button--backStep");
         const addPlaceButton = document.querySelector(".js-button--addPlace");
         const removePlaceButtton = document.querySelector(".js-button--removePlace");
-
+     
+    
         buttonNextStep.addEventListener("click", () => sendFormDataCompany());
-
-
+    
         const sendFormDataCompany = () => {
-            formDataCompany.submit();
-        };
+            checkAndAddEmptyClass();
+            const invalidInputs = document.querySelectorAll(".input.visible.incorrect");
 
+            if (invalidInputs.length > 0) {
+                alert("Wypełnij wszystkie formularze" ,invalidInputs.length);
+                console.log("liczba wierszy niewypelnionych ",  invalidInputs.length );
+            } else {
+               formDataCompany.submit();
+            }
+        };
+    
         buttonBackStep.addEventListener("click", () => sendFormBackStep());
         const sendFormBackStep = () => {
             formBackStep.submit();
         };
-
+    
         addPlaceButton.addEventListener("click", () => showHiddenPlace());
-
+    
         const showHiddenPlace = () => {
             if (currentSectionIndex < sectionsPlace.length - 1) {
-               currentSectionIndex++;
-                sectionsPlace[currentSectionIndex].style.display = "block";
-                console.log("dodalem point" ,currentSectionIndex);
+                currentSectionIndex++;
+                sectionsPlace[currentSectionIndex].style.display = "grid";
+                sectionsPlace[currentSectionIndex].classList.remove('hidden');
+                const inputs = sectionsPlace[currentSectionIndex].querySelectorAll(".input");
+           
+                inputs.forEach(input => {
+                    input.classList.remove("invisible");
+                    input.classList.add("visible");
+                });
+
+              
             };
         };
     
-
         removePlaceButtton.addEventListener("click", () => {
-           if(currentSectionIndex > 0) {
-            sectionsPlace[currentSectionIndex].style.display ="none"
-            currentSectionIndex--;
-            console.log("usunąłem Punkt", currentSectionIndex);
-           };
-
-
+            if (currentSectionIndex > 0) {
+                // Ukrywamy bieżącą sekcję
+                sectionsPlace[currentSectionIndex].style.display = "none";
+                sectionsPlace[currentSectionIndex].classList.add("hidden");
+        
+                // Znajdujemy inputy w bieżącej sekcji i dodajemy im klasę 'visible'
+                const inputs = sectionsPlace[currentSectionIndex].querySelectorAll(".input");
+                inputs.forEach(input => {
+                    input.classList.remove("visible"); // Usuwamy klasę 'invisible'
+                    input.classList.add("invisible"); // Dodajemy klasę 'visible'
+                });
+        
+                currentSectionIndex--; // Zmniejszamy indeks bieżącej sekcji
+        
+                console.log("usunąłem Punkt", currentSectionIndex);
+            }
         });
-
+        
+    
+        const checkAndAddEmptyClass = () => {
+            sectionsPlace.forEach(section => {
+               
+                    const inputs = section.querySelectorAll(".input.visible");
+                    console.log(inputs);
+                    inputs.forEach(input => {
+                        console.log(input);
+                        if (input.value.trim() !== "") {
+                            input.classList.remove("incorrect");
+                            input.classList.add("correct");
+                        } else {
+                            input.classList.add("incorrect");
+                            input.classList.remove("correct");
+                        }
+                    });
+                
+            });
+        };
     });
 }
+
