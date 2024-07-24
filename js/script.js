eda16cce22864441d08de25ee13fc708cb80c617
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-07-24 14:47:46 +0200

    change javascript

--- js/script.js
+++ js/script.js
@@ -3,79 +3,69 @@
 
 
 {
-const paragrafDate = document.querySelector('.paragraf--date');
- 
-// Utwórz obiekt daty
-const currentDate = new Date();
- 
-// Pobierz składowe daty
-const day = currentDate.getDate();
-const month = currentDate.getMonth() + 1; // Dodaj 1, ponieważ styczeń to 0
-const year = currentDate.getFullYear();
- 
-// Formatuj datę na "DD.MM.RRRR"
-const formattedDate = `${day}.${month}.${year} r.`;
- 
-const updateDate = () => {
- 
-    paragrafDate.value = formattedDate;
- 
-};
- 
-// Wywołaj funkcję
-updateDate();
- 
- 
-   
- 
-    var printButton = document.getElementById('printButton');
- 
+
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const paragrafDate = document.querySelector('.paragraf--date');
+        const currentDate = new Date();
+        const day = currentDate.getDate();
+        const month = currentDate.getMonth() + 1; // Dodaj 1, ponieważ styczeń to 0
+        const year = currentDate.getFullYear();
+        const formattedDate = `${day}.${month}.${year} r.`;
+
+        const updateDate = () => {
+            paragrafDate.value = formattedDate;
+        };
+        updateDate();
+
+    });
+
+    const printButton = document.getElementById('printButton');
+
     printButton.addEventListener('click', function () {
-       prepareForPrint();
-       window.print();
-       restoreAfterPrint();
-   });
- 
-    function prepareForPrint() {
-        var contentToPrint = document.getElementById('printContent');
-        contentToPrint.style.display = 'block';
- 
-        // Wyłączanie tytułu strony przed drukowaniem
-        document.title = ".";
- 
- 
- 
+        prepareForPrint();
+        window.print();
+        restoreAfterPrint();
+    });
+
+    const buttonPrintToPdf = document.getElementById('printToPdf');
+    buttonPrintToPdf.addEventListener('click', function () {
+
+        prepareForPrint();
+        saveAsPdf();
+        restoreAfterPrint();
+    });
+
+    function saveAsPdf() {
+        const contentToPrint = document.getElementById('printContent');
+        contentToPrint.style.border = 'none';
+        html2canvas(contentToPrint).then(canvas => {
+            const imgData = canvas.toDataURL('image/png');
+            const { jsPDF } = window.jspdf;
+            const doc = new jsPDF();
+            doc.addImage(imgData, 'PNG', 10, 10, 180, canvas.height * 180 / canvas.width);
+            doc.save('wypowiedzenie.pdf');
+        });
     }
- 
-    function restoreAfterPrint() {
-        var contentToPrint = document.getElementById('printContent');
+
+    function prepareForPrint() {
+        const contentToPrint = document.getElementById('printContent');
         contentToPrint.style.display = 'block';
- 
-        // Przywracanie tytułu strony po drukowaniu
+        document.title = ".";
+
+    }
+
+    function restoreAfterPrint() {
+        const contentToPrint = document.getElementById('printContent');
+        contentToPrint.style.display = 'block';
+
         document.title = "Print Page";
     }
- 
-    function selectOption(option) {
-        document.getElementById('selectedOption').textContent = option;
- 
- 
-    }
- 
-    function toggleDropdown() {
-        var dropdownOptions = document.getElementById('dropdownOptions');
-        dropdownOptions.style.display = dropdownOptions.style.display === 'block' ? 'none' : 'block';
- 
-    }
- 
- 
-    function closeDropdown() {
-        var dropdownOptions = document.getElementById('dropdownOptions');
-        dropdownOptions.style.display = 'none';
-    }
- 
- 
+
+
+
 }
 
 
 
- 
