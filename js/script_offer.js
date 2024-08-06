986bc2f900d9e929a948c6a2a790f96720fec87e
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-08-06 20:06:29 +0200

    ofer test

--- js/script_offer.js
+++ js/script_offer.js
@@ -18,6 +18,9 @@ function updateTotalCost() {
     const monthsOfRentExemption = parseFloat(document.getElementById("sqm-f215")?.textContent) || 0;
     const priceInstalationTerminal = parseFloat(document.getElementById("sqm-f233")?.textContent) || 0;
     const fixedMonthlyCosts = (terminalCostStacjonarny + terminalCostPrzenosny + pinPadCost + gprsCost + serviceCost + logoCost);
+    const timeOfCooperation = parseFloat(document.getElementById("sqm-f327").value) || 0;
+
+
 
     const fixedMonthlyCostsCompetition = parseFloat(document.getElementById("sqm-f324").value) || 0;
     const monthsOfRentExemptionCompetition = parseFloat(document.getElementById("sqm-f325").value) || 0;
@@ -27,28 +30,40 @@ function updateTotalCost() {
     const priceInstalationTerminalCompetition = parseFloat(document.getElementById("sqm-f323").value) || 0;
 
 
-   
-    
- 
-    
-    const monthlyCommission = (fixedMonthlyCosts - monthsOfRentExemption * fixedMonthlyCosts + authorizationFee * ((monthlyCardTurnover - exemptRevenue) / averageTransaction) + (((monthlyCardTurnover - exemptRevenue) * (commissionRate / 100))));
+    const monthlyCommission = (timeOfCooperation * monthlyCardTurnover - exemptRevenue >= 0)
+        ? Math.max(0,
+            authorizationFee * monthlyCardTurnover / averageTransaction + monthlyCardTurnover * commissionRate / 100
+        )
+        : 0;
+    const amountOfCommissionFromExemptTurnover = (timeOfCooperation * monthlyCardTurnover) - exemptRevenue >= 0
+        ? Math.max(0, (exemptRevenue / averageTransaction) * authorizationFee + (exemptRevenue * commissionRate) / 100)
+        : 0;
+    const monthlyTotalCost = Math.max(0, monthlyCommission + fixedMonthlyCosts - (monthsOfRentExemption * fixedMonthlyCosts)).toFixed(2);
+    const totalCost = ((priceInstalationTerminalCompetition + (timeOfCooperation * monthlyCommission) - amountOfCommissionFromExemptTurnover + Math.max(0, (timeOfCooperation - monthsOfRentExemption) * fixedMonthlyCosts))).toFixed(2);
 
-    const monthlyCommissionCompetition = (fixedMonthlyCostsCompetition - monthsOfRentExemptionCompetition * fixedMonthlyCostsCompetition + authorizationFeeCompetition * ((monthlyCardTurnover - exemptRevenueCompetition) / averageTransaction) + (((monthlyCardTurnover - exemptRevenueCompetition) * (commissionRateCompetition / 100))));
+    const monthlyCommissionCompetition = (timeOfCooperation * monthlyCardTurnover - exemptRevenueCompetition >= 0)
+        ? Math.max(0,
+            authorizationFeeCompetition * monthlyCardTurnover / averageTransaction + monthlyCardTurnover * commissionRateCompetition / 100
+        )
+        : 0;
+    const amountOfCommissionFromExemptTurnoverCompetition = (timeOfCooperation * monthlyCardTurnover) - exemptRevenueCompetition >= 0
+        ? Math.max(0, (exemptRevenueCompetition / averageTransaction) * authorizationFeeCompetition + (exemptRevenueCompetition * commissionRateCompetition) / 100)
+        : 0;
+    const monthlyTotalCostCompetition = Math.max(0, monthlyCommissionCompetition + Math.max(0, fixedMonthlyCostsCompetition - (monthsOfRentExemptionCompetition * fixedMonthlyCostsCompetition))).toFixed(2);
+    const totalCostCompetition = ((priceInstalationTerminalCompetition + (timeOfCooperation * monthlyCommissionCompetition) - amountOfCommissionFromExemptTurnoverCompetition + Math.max(0, (timeOfCooperation - monthsOfRentExemptionCompetition) * fixedMonthlyCostsCompetition))).toFixed(2);
 
-    const totalCost = (priceInstalationTerminal + 12 * parseFloat(monthlyCommission)).toFixed(2);
-    const totalCostCompetition  = (priceInstalationTerminalCompetition  + 12 * parseFloat(monthlyCommissionCompetition )).toFixed(2);
 
-const differencePrice =  totalCostCompetition - totalCost + " zł "
+    const differencePrice = (totalCostCompetition - totalCost).toFixed(2) + " zł ";
 
     document.getElementById("fixed-monthly-costs").textContent = fixedMonthlyCosts;
-    document.getElementById("monthly-commission").value = monthlyCommission;
-    document.getElementById("monthly-commission-competition").value = monthlyCommissionCompetition;
-    document.getElementById("different-price").value = differencePrice;
+    document.getElementById("monthly-total-cost").value = monthlyTotalCost;
+    document.getElementById("monthly-total-cost-competition").value = monthlyTotalCostCompetition;
+    document.getElementById("sqm-f328").value = differencePrice;
 
     document.getElementById("sqm-f321").value = totalCost + " zł";
     document.getElementById("sqm-f322").value = totalCostCompetition + " zł";
 
-  
+
 
     console.log("koszt terminal stacjonarny :", terminalCostStacjonarny);
     console.log("koszt terminal przenosny  :", terminalCostPrzenosny);
@@ -64,24 +79,47 @@ const differencePrice =  totalCostCompetition - totalCost + " zł "
     console.log("opłata autoryzacyjna:", authorizationFee);
     console.log("średnia wartośc transakcji", averageTransaction);
     console.log("opłata instalacyjna ", priceInstalationTerminal);
+    console.log("Czas współpracy liczba miesieczy ", timeOfCooperation);
     console.log("Suma Miesięczne opłaty stałe:", fixedMonthlyCosts);
     console.log("Całe Miesięczne opłaty :", monthlyCommission);
     console.log("Całkowity 12 miesięczny koszt:", totalCost);
 
     console.log("liczba zwolnienia z opłat konkurencja", monthsOfRentExemptionCompetition);
     console.log("Miesięczne płaty stałe konkurencja", fixedMonthlyCostsCompetition);
-    
     console.log("prowizja konkurencja:", commissionRateCompetition);
     console.log("zwolniona prowizja konkurencja:", exemptRevenueCompetition);
     console.log("opłata autoryzacyjna konkurencja:", authorizationFeeCompetition);
-      console.log("opłata instalacyjna konkurencja:", priceInstalationTerminalCompetition);
+    console.log("opłata instalacyjna konkurencja:", priceInstalationTerminalCompetition);
     console.log("Miesięczne opłaty stałe: konkurencja", fixedMonthlyCostsCompetition);
-    console.log("Całe miesieczne  Miesięczne opłaty konkurencja :", monthlyCommissionCompetition);
+    console.log("Całe prowizja  konkurencja :", monthlyCommissionCompetition);
+    console.log("kwota prowizji zwolnionej", amountOfCommissionFromExemptTurnoverCompetition);
+    console.log("Całkowity 12 miesięczny koszt konkurencja :", totalCostCompetition);
 
-    console.log("Całkowity 12 miesięczny koszt :", totalCostCompetition);
+    event.preventDefault();
+    
+    const formCalculate = document.querySelector(".js-form--calculate");
+    console.log(formCalculate);
+
+   
+    formCalculate.submit();
 
 };
 
 const buttonRecalculate = document.querySelector(".js-button");
 buttonRecalculate.addEventListener("click", updateTotalCost);
+
+
+const buttonNextStep = document.querySelector(".js-nextStep");
+buttonNextStep.addEventListener("click", () => {
+    event.preventDefault();
+
+    const formNextStep = document.querySelector(".js-form--DataCompany");
+    console.log(buttonNextStep);
+
+   console.log(formNextStep);
+    formNextStep.submit();
+
+    
+
+});
 
