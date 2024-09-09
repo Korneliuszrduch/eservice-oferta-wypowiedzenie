504368ac95d66f0c6f9abec21b8599bdedc04229
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-09-09 20:47:42 +0200

    another change

--- php/display_users.php
+++ php/display_users.php
@@ -1,25 +1,42 @@
 <?php
-include 'php/config.php';
+include 'config.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-// Zmodyfikowane zapytanie SQL
+// Pobieranie liczby SID-ów do wyświetlenia z formularza
+$limit = isset($_POST['number_of_sids']) ? intval($_POST['number_of_sids']) : 200; // Domyślnie 10, jeśli brak wartości
+
+// Ustawienie minimalnej wartości limitu na 1, aby uniknąć problemów
+$limit = max(1, $limit);
+
+// Przygotowanie zapytania SQL z użyciem parametrów
 $sql = "SELECT u.sid, u.name_first, u.email, u.phone, 
                MAX(CASE WHEN f.fid = 171 THEN f.value END) AS field_value_171, 
                MAX(CASE WHEN f.fid = 116 THEN f.value END) AS field_value_116,
-               MAX(CASE WHEN f.fid = 115 THEN f.value END) AS field_value_115
+               MAX(CASE WHEN f.fid = 115 THEN f.value END) AS field_value_115,
+               MAX(CASE WHEN f.fid = 120 THEN f.value END) AS field_value_120
         FROM nm_krduch2subscribers u
         LEFT JOIN nm_krduch2subscribers_fields f ON u.sid = f.sid
-        WHERE u.sid <= 8590 AND u.status = 'active'
+        WHERE u.status = 'active'
         GROUP BY u.sid, u.name_first, u.email, u.phone
-        ORDER BY u.sid DESC
-        LIMIT 200";
+       ORDER BY u.sid DESC
+        LIMIT ?";
 
-$result = $conn->query($sql);
+// Przygotowanie zapytania
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $limit); // Binding limit as an integer
 
+// Wykonanie zapytania
+$stmt->execute();
+$result = $stmt->get_result();
+
+// Sprawdzenie, czy zapytanie zwróciło wyniki
 if ($result->num_rows > 0) {
     echo "<table border='1'>
             <thead>
                 <tr>
                     <th>SID</th>
+                     <th>NIP</th>
                     <th>Imię</th>
                     <th>Email</th>
                     <th>Telefon</th>
@@ -30,7 +47,7 @@ if ($result->num_rows > 0) {
                 </tr>
             </thead>
             <tbody>";
-    
+
     while ($row = $result->fetch_assoc()) {
         $currentDate = date('Y-m-d'); // Format: RRRR-MM-DD
         $combinedValue = $row['field_value_116'] . ' ' . $currentDate;
@@ -39,6 +56,7 @@ if ($result->num_rows > 0) {
         echo "<tr class='data-table'>
                 <form class='form' accept-charset='UTF-8' action='https://mail.korneliuszrduch.pl/subscribe.php' method='POST'>
                     <td>" . htmlspecialchars($row['sid'], ENT_QUOTES, 'UTF-8') . "</td>
+                      <td><input type='text' name='fields[120]' value='" . htmlspecialchars($row['field_value_120'], ENT_QUOTES, 'UTF-8') . "'></td>
                     <td><input type='text' name='fname' value='" . htmlspecialchars($row['name_first'], ENT_QUOTES, 'UTF-8') . "'></td>
                     <td><input type='email' name='email' value='" . htmlspecialchars($row['email'], ENT_QUOTES, 'UTF-8') . "'></td>
                     <td><a href='tel:" . htmlspecialchars($row['phone'], ENT_QUOTES, 'UTF-8') . "'>
@@ -52,6 +70,17 @@ if ($result->num_rows > 0) {
                             <option value='0% 2 Brak kontaktu'" . ($selectedOption === '0% 2 Brak kontaktu' ? ' selected' : '') . ">0% 2 Brak kontaktu</option>
                             <option value='0% 3 Brak kontaktu'" . ($selectedOption === '0% 3 Brak kontaktu' ? ' selected' : '') . ">0% 3 Brak kontaktu</option>
                             <option value='0% SMS'" . ($selectedOption === '0% SMS' ? ' selected' : '') . ">0% SMS</option>
+
+                                <option value='10% Omówić potrzeby'" . ($selectedOption === '10% Omówić potrzeby' ? ' selected' : '') . ">10% Omówić potrzeby</option>
+                             <option value='14% Przygotować kalkulator'" . ($selectedOption === '14% Przygotować kalkulator' ? ' selected' : '') . ">14% Przygotować kalkulator</option>
+                            <option value='18% Przedstawić ofertę'" . ($selectedOption === '18% Przedstawić ofertę' ? ' selected' : '') . ">18% Przedstawić ofertę</option>
+                              <option value='0%08 Obecny klient eservice'" . ($selectedOption === '0%08 Obecny klient eservice' ? ' selected' : '') . ">0%08 Obecny klient eservice</option>
+
+                          
+
+
+                           
+                          
                         </select>
                     </td>
                     <td><textarea name='fields[116]' rows='4' cols='25'>" . htmlspecialchars($combinedValue, ENT_QUOTES, 'UTF-8') . "</textarea></td>
@@ -71,5 +100,7 @@ if ($result->num_rows > 0) {
     echo "<tr><td colspan='8'>Brak użytkowników w bazie danych.</td></tr>";
 }
 
+// Zamknięcie zapytania i połączenia
+$stmt->close();
 $conn->close();
 ?>
--- php/register.php
+++ php/register.php
@@ -1,7 +1,5 @@
 <?php
-
-
-
+session_start();
 include '../php/config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -12,19 +10,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Sprawdzenie, czy wszystkie wymagane pola są wypełnione
     if (!empty($phone)) {
-        // Przygotowanie i wykonanie zapytania SQL
-        $stmt = $conn->prepare("INSERT INTO nm_krduch2subscribers (name_first, email, phone) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $name_first, $email, $phone);
+        // Sprawdzenie, czy telefon już istnieje w bazie danych
+        $stmt = $conn->prepare("SELECT sid, name_first, email FROM nm_krduch2subscribers WHERE phone = ?");
+        $stmt->bind_param("s", $phone);
+        $stmt->execute();
+        $stmt->store_result();
 
-        if ($stmt->execute()) {
-            echo "Nowy użytkownik został zarejestrowany.";
+        if ($stmt->num_rows > 0) {
+            // Telefon istnieje, pobranie imienia i emaila
+            $stmt->bind_result($sid, $name_first, $email);
+            $stmt->fetch();
+            
+            // Przechowywanie danych w sesji
+            $_SESSION['name_first'] = $name_first;
+            $_SESSION['email'] = $email;
+            $_SESSION['phone'] = $phone;
+            $_SESSION['message'] = "Telefon już istnieje w bazie. SID: " . $sid;
+            
+            // Przekierowanie na stronę z formularzem
+            header("Location: /indexsqldate.php");
+            exit();
         } else {
-            echo "Błąd: " . $stmt->error;
+            // Telefon nie istnieje, zapisz nowego użytkownika
+
+            $created = time(); // Pobierz aktualny timestamp
+
+            $stmt = $conn->prepare("INSERT INTO nm_krduch2subscribers (name_first, email, phone, status, created) VALUES (?, ?, ?, 'active', ?)");
+            $stmt->bind_param("ssss", $name_first, $email, $phone, $created);
+
+
+            if ($stmt->execute()) {
+                $_SESSION['message'] = "Nowy użytkownik został zarejestrowany.";
+                $_SESSION['name_first'] = '';
+                $_SESSION['email'] = '';
+                $_SESSION['phone'] = '';
+            } else {
+                $_SESSION['message'] = "Błąd: " . $stmt->error;
+            }
+            
+            // Przekierowanie na stronę z formularzem
+            header("Location: /indexsqldate.php");
+            exit();
         }
 
         $stmt->close();
     } else {
-        echo "Proszę wypełnić wszystkie wymagane pola.";
+        $_SESSION['message'] = "Proszę wypełnić wszystkie wymagane pola.";
+        header("Location: /indexsqldate.php");
+        exit();
     }
 
     $conn->close();
