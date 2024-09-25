3ff33a3ed810983025bd0908857ef8daa7ecee84
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-09-25 18:09:47 +0200

    Add code with hotjar

---
+++ js/monitoringsite.js
@@ -0,0 +1,19 @@
+
+{
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5147710,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+
+    window.smartlook||(function(d) {
+        var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+        var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+        c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+        })(document);
+        smartlook('init', 'c88109efaec4c8b81ed2a1e1bbb4f03ed7fb9640', { region: 'eu' });
+}
