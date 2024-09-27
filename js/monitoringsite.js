53d863bb5f50b5c90e84c7c35b4c0240fc8a27f1
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-09-27 18:31:41 +0200

    add tag menager code

--- js/monitoringsite.js
+++ js/monitoringsite.js
@@ -1,5 +1,16 @@
 
 {
+
+
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || []; w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        }); var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-TB5HRX4');
+
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:5147710,hjsv:6};
@@ -16,4 +27,6 @@
         c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
         })(document);
         smartlook('init', 'c88109efaec4c8b81ed2a1e1bbb4f03ed7fb9640', { region: 'eu' });
+
+   
 }
