7f6c4b89ecf7042c8c670bff6a908297720694f2
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-10-19 15:18:53 +0200

    add funkction  highlightTheCustomerDidNotOpenOffers

--- js/crm.js
+++ js/crm.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.js-delete-button').forEach(function (button) {
-        button.addEventListener('click', function (event) {
+    // Usuwanie subskrybenta
+
+
+    const rows = Array.from(document.querySelectorAll(".js-data-table"));
+    const emailForUrl = new URLSearchParams(window.location.search).get('mail'); // Get email from URL
+    const currentDate = new Date();
+
+    document.querySelectorAll('.js-delete-button').forEach(button => {
+        button.addEventListener('click', event => {
             event.preventDefault();
-            const sid = this.dataset.sid;
+            const sid = button.dataset.sid;
             if (confirm("Czy na pewno chcesz usunąć tego subskrybenta?")) {
                 fetch('php/delete_subscriber.php', {
                     method: 'POST',
@@ -15,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     .then(data => {
                         if (data.success) {
                             alert("Subskrybent został usunięty.");
-                            this.closest('tr').remove();
+                            button.closest('tr').remove();
                         } else {
                             alert("Wystąpił błąd: " + data.message);
                         }
@@ -29,15 +36,97 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 
-    const buttonSaveDateContact = document.querySelectorAll(".js-save-button");
+    const hideContactBecauseDone = () => {
+        const dataTableAll = document.querySelectorAll(".js-data-table");
+        dataTableAll.forEach(dataTable => {
+            const latestCustomerStatus = dataTable.querySelector(".js-latest-custumer-status");
+            const inputDate = dataTable.querySelector('.js-date-time-local');
+            if (latestCustomerStatus && latestCustomerStatus.value.trim() === "100% Dokumenty wprowadzone") {
 
+                if (inputDate && new Date(inputDate.value) < currentDate) {
+                    dataTable.classList.add("hidden");
+                } else {
+                    dataTable.classList.add("hidden");
+                }
+            }
+        });
+    };
+
+    const sortRows = (rows) => {
+        return rows.sort((a, b) => {
+            const aHighlight = a.classList.contains('highlight');
+            const bHighlight = b.classList.contains('highlight');
+            if (aHighlight && !bHighlight) return -1; // a ma highlight, b nie
+            if (!aHighlight && bHighlight) return 1;  // b ma highlight, a nie
+            const aDate = new Date(a.querySelector('.js-date-time-local')?.value);
+            const bDate = new Date(b.querySelector('.js-date-time-local')?.value);
+            return bDate - aDate;
+        });
+    };
+
+
+    const highlightContact = () => {
+
+        if (!emailForUrl) return;
+
+
+        rows.forEach(dataTable => {
+            const emailInTable = dataTable.querySelector(".js-email-contact")?.value.trim();
+            if (emailInTable === emailForUrl) {
+                dataTable.classList.remove("hidden");
+                dataTable.classList.add("highlight");
+            }
+        });
+
+
+        const tableBody = document.querySelector('.js-tbody');
+        const sortedRows = sortRows(rows);
+        sortedRows.forEach(row => tableBody.appendChild(row));
+    };
+
+
+    const showContacts = () => {
+        const inputMinuts = document.querySelector(".js-time-for-show");
+        const minutes = parseInt(inputMinuts.value, 10) || 0;
+        const currentDate = new Date(Date.now() - minutes * 60 * 1000);
+        const rows = Array.from(document.querySelectorAll('.js-data-table'));
+        hideRowsByTimeContact(rows, currentDate, emailForUrl);
+
+    };
+
+
+
+    const hideRowsByTimeContact = (rows, currentDate, emailForUrl) => {
+        rows.forEach(row => {
+            const inputDate = row.querySelector('.js-date-time-local');
+            const emailInTable = row.querySelector('.js-email-contact')?.value.trim();
+
+
+            if (emailForUrl && emailInTable === emailForUrl) {
+
+                row.classList.remove('hidden');
+            } else if (inputDate && new Date(inputDate.value) > currentDate) {
+
+                row.classList.add('hidden');
+            } else {
+
+                row.classList.remove('hidden');
+            }
+        });
+    };
+
+
+
+
+    // Obsługa przycisku "Pokaż kontakty"
+    const buttonShowContacts = document.querySelector(".js-button-show-contacs");
+    buttonShowContacts.addEventListener("click", showContacts); // Wywołanie funkcji po kliknięciu przycisku
+
+    const buttonSaveDateContact = document.querySelectorAll(".js-save-button");
     buttonSaveDateContact.forEach(button => {
         button.addEventListener("click", event => {
             event.preventDefault();
-            
-         
             const formDateContact = button.closest('tr').querySelector(".js-form-date-contact");
-            
             if (formDateContact) {
                 formDateContact.submit(); // Wysłanie odpowiedniego formularza
             } else {
@@ -45,126 +134,30 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-    
 
-
-    const getParametr = (parametr) => new URLSearchParams(window.location.search).get(parametr);
-    const inputMinuts = document.querySelector(".js-time-for-show");
-    const butonShowContacbeforTime = document.querySelector(".js-button-show-contacs");
-    const allDataTable = document.querySelectorAll(".js-data-table");
-    const wartoscEmail = getParametr('mail');
-    const emailInputs = document.querySelectorAll('.js-input-email');
-    console.log("wartosc mail:", wartoscEmail);
-    console.log("emailInputs:", emailInputs);
-
-    if (wartoscEmail) {
-        emailInputs.forEach(input => {
-            input.value = wartoscEmail;
-        });
-    }
-
-
-
-
-
-
-
-    const showContacs = () => {
-        const minutes = parseInt(inputMinuts.value, 10) || 0;
-        const currentDate = new Date(Date.now() - minutes * 60 * 1000); // Odejmowanie minut
-        console.log("Bieżąca data (pomniejszona o " + minutes + " minut):", currentDate);
-
-        const rows = Array.from(document.querySelectorAll('.js-data-table'));
-
-        // Sortowanie wierszy, aby te z klasą highlight były na górze
-        rows.sort((a, b) => {
-            if (a.classList.contains('highlight') && !b.classList.contains('highlight')) {
-                return -1;
-            }
-            if (!a.classList.contains('highlight') && b.classList.contains('highlight')) {
-                return 1;
-            }
-            return 0;
-        }).forEach(row => {
-            const input = row.querySelector('.js-date-time-local');
-            if (input) {
-                const inputValue = input.value;
-                if (inputValue) {
-                    const inputDate = new Date(inputValue);
-                    if (inputDate > currentDate) {
-                        row.classList.add('hidden');
-                        allDataTable.forEach(dataTable => {
-                            const inputInTable = dataTable.querySelector(".js-email-contact");
-                            if (inputInTable && inputInTable.value === wartoscEmail) {
-                                dataTable.classList.remove("hidden");
-                                dataTable.classList.add("highlight");
-                            }
-                        });
-
-
-                    } else {
-                        row.classList.remove('hidden');
-                    }
-                } else {
-                    row.classList.add('hidden');
-                }
-            } else {
-                row.classList.add('hidden');
-                allDataTable.forEach(dataTable => {
-                    const inputInTable = dataTable.querySelector(".js-email-contact");
-                    if (inputInTable && inputInTable.value === wartoscEmail) {
-                        dataTable.classList.remove("hidden");
-                        dataTable.classList.add("highlight");
-                    }
-                });
-            }
-
-
-            row.parentNode.appendChild(row);
-        });
-    };
-
-    allDataTable.forEach(dataTable => {
-        const inputInTable = dataTable.querySelector(".js-email-contact");
-        if (inputInTable && inputInTable.value === wartoscEmail) {
-            dataTable.classList.remove("hidden");
-            dataTable.classList.add("highlight");
-        }
-    });
-
-    // Wywołanie showContacs po załadowaniu strony
-    showContacs();
-
-    butonShowContacbeforTime.addEventListener("click", () => {
-        showContacs(); // Wywołanie funkcji, kiedy użytkownik kliknie przycisk
-    });
-
-
-
-    const readPhoneNameForUrl = () => {
-
-        const phone = getParametr("phone");
-        const fname = getParametr("fname");
+    // Funkcja odczytująca dane z URL i ustawiająca w odpowiednich polach
+    const readParametersForUrl = () => {
+        const params = new URLSearchParams(window.location.search);
         const inputPhone = document.querySelector('.js-input-phone');
         const inputFname = document.querySelector('.js-input-name-first');
-        if (inputPhone && phone) {
-            inputPhone.value = phone;
-        }
-        if (inputFname && fname) {
-            inputFname.value = fname;
-        }
-    }
+        const inputEmails = document.querySelectorAll('.js-input-email');
+        const inputPlaceOfAcquiringTheCustomer = document.querySelector('.js-input-place-of-acquiring-the-customer');
 
-    const buttonReadPhoneForUrl = document.querySelector(".js-read-phone-url");
-    buttonReadPhoneForUrl.addEventListener("click", readPhoneNameForUrl);
+        if (inputPhone) inputPhone.value = params.get("phone") || '';
+        if (inputFname) inputFname.value = params.get("fname") || '';
+        if (inputPlaceOfAcquiringTheCustomer) inputPlaceOfAcquiringTheCustomer.value = params.get("a") || '';
+        inputEmails.forEach(input => input.value = params.get("mail") || '');
+    };
 
-    const buttonShowContacts = document.querySelector(".js-button-show-hidden");
-    buttonShowContacts.addEventListener("click", () => {
+    // Obsługa przycisku "Pokaż ukryte kontakty"
+    const buttonShowHiddenContacts = document.querySelector(".js-button-show-hidden");
+    buttonShowHiddenContacts.addEventListener("click", () => {
         document.querySelectorAll(".js-data-table").forEach(row => {
             row.classList.remove("hidden");
         });
     });
 
+    // Obsługa przycisku "Ukryj kontakty"
     const buttonHiddenContacts = document.querySelector(".js-button-hidden-contact");
     buttonHiddenContacts.addEventListener("click", () => {
         const currentDate = new Date();
@@ -177,65 +170,12 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-
-
-    readPhoneNameForUrl();
-
-    const ustawAlarm = () => {
-        const teraz = new Date();
-        const elementInputDaty = document.querySelector(".js-date-time-local");
-
-        // Pobranie wartości z inputu (w formacie 'YYYY-MM-DDTHH:MM')
-        const dataKontaktu = new Date(elementInputDaty.value);
-        console.log("Data z inputu: ", dataKontaktu.toString());
-        console.log("Obecny czas: ", teraz.toString());
-
-        // Obliczenie różnicy czasu
-        const roznicaCzasu = dataKontaktu - teraz;
-        console.log("Różnica czasu: ", roznicaCzasu);
-
-        // Sprawdzenie, czy data jest w przyszłości
-        if (roznicaCzasu > 0) {
-            setTimeout(() => {
-                console.log("Wywołanie powiadomienia...");
-                if (Notification.permission === "granted") {
-                    new Notification("Przypomnienie", {
-                        body: "Masz zaplanowany kontakt na teraz!",
-                    });
-                } else {
-                    alert("Masz zaplanowany kontakt na teraz!");
-                }
-            }, 10000);  // 10 sekund na test
-        } else {
-            alert("Wybrana data musi być w przyszłości.");
-        }
-    };
-
-    // Sprawdzenie, czy powiadomienia są wspierane i dozwolone
-    if ("Notification" in window) {
-        console.log("API powiadomień jest wspierane.");
-        if (Notification.permission === "default") {
-            Notification.requestPermission().then(permission => {
-                console.log("Status uprawnień powiadomień: ", permission);
-            });
-        } else {
-            console.log("Uprawnienia powiadomień są już udzielone lub odmówione.");
-        }
-    }
-
-    const buttonSetAlarm = document.querySelector(".js-set-alarm");
-    buttonSetAlarm.addEventListener("click", ustawAlarm);
-
-
-
-
-
     const registerUpdateButton = document.querySelector('.js-button-register-update-contact');
     registerUpdateButton.addEventListener('click', () => {
         const nameFirst = document.querySelector('.js-input-name-first').value.trim();
         const email = document.querySelector('.js-input-email').value.trim();
         const phone = document.querySelector('.js-input-phone').value.trim();
-        const isUpdate = document.querySelector('.js-button-register-update-contact').dataset.update === 'true';
+        const isUpdate = registerUpdateButton.dataset.update === 'true';
 
         if (email) {
             const formData = new FormData();
@@ -251,21 +191,41 @@ document.addEventListener('DOMContentLoaded', () => {
                 .then(response => response.text())
                 .then(result => {
                     console.log('Server response:', result); // Logowanie odpowiedzi serwera dla debugowania
-
-                    // Odswiezanie strony crm.php z parametrem email
                     window.location.href = `https://terminal.terminaleservice.pl/crm.php?mail=${encodeURIComponent(email)}`;
                 })
                 .catch(error => console.error('Error:', error));
         } else {
-            console.error('Proszę wypełnić wymagane pola.'); // Można wyświetlić komunikat w inny sposób niż alert
+
+            alert('Proszę wypełnić wymagane pola');
         }
     });
 
+    const highlightTheCustomerDidNotOpenOffers = () => {
+        const dataTableAll = document.querySelectorAll(".js-data-table");
+    
+        dataTableAll.forEach(dataTable => {
+            const offerSentStatus = dataTable.querySelector(".js-offer-sent-status").value;
+            const offerOpeningStatus = dataTable.querySelector(".js-offer-opening-status").value; // Używamy querySelector
+    
+            // Sprawdzamy, czy oferta została wysłana i czy oferta nie została otwarta
+            if (offerSentStatus === "Ofertę wysłano" && offerOpeningStatus === "") {
+                dataTable.classList.add("highlight-red");
+                console.log("status wysyłki", offerSentStatus, "status otwarcia", offerOpeningStatus);
+            } else {
+                dataTable.classList.remove("highlight-red");
+            }
+        });
+    };
+    
+
+    // Inicjalizacja
+    readParametersForUrl();
 
 
-
-
-
-
+    hideRowsByTimeContact(rows, currentDate, emailForUrl);
+    hideContactBecauseDone();
+    highlightContact();
+    highlightTheCustomerDidNotOpenOffers();
+    //showContacts();
 
 });
