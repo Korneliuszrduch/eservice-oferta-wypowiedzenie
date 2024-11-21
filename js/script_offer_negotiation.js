41b890a4401f98e78c89e88c2bd20603e9ee43c9
Korneliusz Rduch <korneliusz.rduch@gmail.com>
2024-11-21 19:34:58 +0100

    clearly code

--- js/script_offer_negotiation.js
+++ js/script_offer_negotiation.js
@@ -3,49 +3,6 @@
     document.addEventListener('DOMContentLoaded', () => {
 
 
-        /*  const handleSectionVisibility = (entries, observer) => {
-              entries.forEach(entry => {
-                  if (entry.isIntersecting) {
-                      alert("Zaoszczędziłeś!");
-                      observer.unobserve(entry.target); // Po wywołaniu alertu przestajemy obserwować sekcję
-                  }
-              });
-          };
-      
-          // Konfiguracja IntersectionObserver
-          const observer = new IntersectionObserver(handleSectionVisibility, {
-              root: null, // Przestrzeń widoku przeglądarki
-              threshold: 0.1 // Próg, przy którym wywołuje się zdarzenie (10% widoczności)
-          });
-      
-          // Znalezienie sekcji do obserwowania
-          const summarySection = document.querySelector(".container__section--summary");
-          if (summarySection) {
-              observer.observe(summarySection);
-          }
-      
-      
-      */
-
-
-        loadRandomLinkJs = () => {
-            const randomNum = Math.floor(Math.random() * 10000); // Generuje losową liczbę między 0 a 9999
-
-            // Tworzenie nowego elementu <script> z klasą
-            const newScript = document.createElement('script');
-            newScript.src = `https://terminal.terminaleservice.pl/js/script_offer.js?${randomNum}`;
-            newScript.defer = true;
-            newScript.className = 'js-script-offer'; // Dodanie klasy do nowego skryptu
-        
-            // Dodanie nowego skryptu do dokumentu
-            document.body.appendChild(newScript);
-        
-            // Usunięcie obecnego skryptu, aby zapobiec duplikatom
-            const currentScript = document.querySelector('script[src*="script_offer.js"]');
-            if (currentScript) {
-                currentScript.remove();
-            }
-        }
 
 
 
@@ -56,11 +13,57 @@
             commissionRateOrange.textContent.trim() === ""
                 ? sectionCommisionPhoneCard.classList.add("hidden")
                 : sectionCommisionPhoneCard.classList.remove("hidden");
-
-
-
         };
 
+
+        const setSideForProductType = () => {
+            const typeProduct = document.querySelector(".js-product-type").textContent.trim();
+            const sectionQuestionsAplication = document.querySelector(".js-questions-to-aplication");
+            const sectionQuestionsterminals = document.querySelector(".js-questions-terminals");
+            const sectionImageTerminals = document.querySelector(".js-offer-section-image-terminals");
+            const sectionService = document.querySelector(".js-offer-section-service");
+            const sectionIntro = document.querySelector(".js-offer-section-intro-for-terminals");
+            const sectionImageIntroAplication = document.querySelector(".js-offer-section-image-intro-for-aplication");
+            const sectionImageAplication = document.querySelector(".js-offer-section-image-for-aplication");
+            const sectionImagesTerminals = document.querySelector(".js-offer-section-image-for-terminals");
+            const sectionImagesStepsAplication = document.querySelector(".js-offer-section-image-steps-for-aplication")
+            const priceTerminalSmartPos = document.querySelector(".js-price-smartpos");
+            const priceTerminalStacionary = document.querySelector(".js-price-terminal-stationary");
+            const priceTerminalPortable = document.querySelector(".js-price-terminal-portable");
+            const pricePinpad = document.querySelector(".js-price-pinpad");
+            const aplicationEserviceTom = document.querySelector(".js-aplication-eservicet-tom").textContent.trim();
+            const sectionOneTimeFeesApliction = document.querySelector(".js-one-time-fees-aplication");
+            const sectionOneTimeFeesTerminals = document.querySelector(".js-one-time-fees-terminals");
+            const sectionWideoForAplication = document.querySelector(".js-offer-section-wideo-for-aplication");
+
+            const hasPrice = priceTerminalSmartPos.textContent.trim() ||
+                priceTerminalStacionary.textContent.trim() ||
+                priceTerminalPortable.textContent.trim() ||
+                pricePinpad.textContent.trim();
+
+            if (aplicationEserviceTom === "Aplikacja terminalowa eService Tom") {
+                // sectionQuestionsAplication.classList.remove("hidden");
+                //  sectionImageAplication.classList.remove("hidden");
+                //  sectionImagesStepsAplication.classList.remove("hidden");
+                // sectionImageIntroAplication.classList.remove("hidden");
+                sectionOneTimeFeesApliction.classList.remove("hidden");
+                // sectionWideoForAplication.classList.remove("hidden");
+            }
+
+            if (hasPrice) {
+                console.log("pinpad cena", pricePinpad.textContent,
+                    "cena stacjonarny", priceTerminalStacionary.textContent,
+                    "cena przenośny", priceTerminalPortable.textContent,
+                    "cena smart pos", priceTerminalSmartPos.textContent);
+
+                //  sectionIntro.classList.remove("hidden");
+                //  sectionService.classList.remove("hidden");
+                // sectionImageTerminals.classList.remove("hidden");
+                //  sectionQuestionsterminals.classList.remove("hidden");
+                // sectionImagesTerminals.classList.remove("hidden");
+                sectionOneTimeFeesTerminals.classList.remove("hidden");
+            }
+        };
         const ShowIntegrationTheCashRegisterSystem = () => {
 
             const nameIntegrationTheCashRegisterSystem = document.querySelector(".js-name-cash-register-system");
@@ -69,133 +72,29 @@
                 ? sectionIntegrationTheCashRegisterSystem.classList.add("hidden")
                 : sectionIntegrationTheCashRegisterSystem.classList.remove("hidden");
 
-
-
         };
-
-
-
-
-
-      
-        
-
-       
-      
-
-
-       
-
-
-
-    
-
-       
-
-
-     
-
-       
-
-     
-
-
-
-       
-
-       
-       
-
-       
-
-
-
-
-      
-
-
-
-
-
-
-       
-
-      
-
-
-       
-
-
-
-
-        
-        const sendFormDataCompany = () => {
-            checkAndAddEmptyClass();
-            const formCalculate = document.querySelector(".js-form--calculate");
-            const invalidInputs = document.querySelectorAll(".container__input.incorrect");
-            if (invalidInputs.length > 0) {
-                alert("Wypełnij wszystkie formularze", invalidInputs.length);
-
-            } else {
-                setMemoryOptions();
-                formCalculate.submit();
-            }
-        };
-
-
-        const checkAndAddEmptyClass = () => {
-            const containerConnersion = document.querySelectorAll(".container--conversion");
-
-            containerConnersion.forEach(section => {
-                const inputs = section.querySelectorAll(".container__input");
-                //console.log("Section Inputs:", inputs);
-
-                inputs.forEach(input => {
-                    // console.log("Current Input:", input); // Wyświetla aktualnie przetwarzany input
-
-                    // Sprawdzenie, czy sekcja jest ukryta
-                    if (input.classList.contains("hidden")) {
-                        input.classList.remove("incorrect");
-                        input.classList.add("correct");
-                        //  console.log("Hidden: Input set to correct:", input);
-                    } else {
-                        // Sprawdzenie, czy input jest pusty
-                        if (input.value.trim() !== "") {
-                            input.classList.remove("incorrect");
-                            input.classList.add("correct");
-                            // console.log("Visible: Input set to correct:", input);
-                        } else {
-                            // Jeżeli input jest pusty i widoczny, dodaj klasę incorrect
-                            input.classList.add("incorrect");
-                            input.classList.remove("correct");
-                            // console.log("Visible: Input set to incorrect:", input);
-                        }
-                    }
-                });
-            });
-        };
-
-
-       
-
-
 
 
         const buttonNextStep = document.querySelector(".js-nextStep");
-        buttonNextStep.addEventListener("click", () => {
-            event.preventDefault();
 
-            const formNextStep = document.querySelector(".js-form--DataCompany");
-            // console.log(buttonNextStep);
+        if (buttonNextStep) {
+            buttonNextStep.addEventListener("click", (event) => {
+                event.preventDefault();
+                const formNextStep = document.querySelector(".js-form--recommended");
+                if (formNextStep) {
 
-            // console.log(formNextStep);
-            formNextStep.submit();
-
-
-
-        });
-
-
+                    console.log("formularz", formNextStep);
+                    formNextStep.submit();
+                    setTimeout(() => {
+                        formNextStep.submit();
+                    }, 300);
+                } else {
+                    console.error("Nie znaleziono formularza z klasą .js-form--recommended");
+                }
+            });
+        } else {
+            console.error("Nie znaleziono przycisku z klasą .js-nextStep");
+        }
 
 
         const loadStatusOpenOffer = () => {
@@ -204,53 +103,42 @@
                 console.error("Element z klasą .js-email nie został znaleziony.");
                 return;
             }
-        
+
             const email = emailElement.value;
-        
             const formData = new FormData();
             formData.append('email', email);
             formData.append('offer-opening-status', 'kliknął w ofertę lub dokument');
-        
+
             fetch('https://terminal.terminaleservice.pl/php/update_fields.php', {
                 method: 'POST',
                 body: formData
             })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.text();
-            })
-            .then(result => {
-                console.log('Status updated:', result);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('Wystąpił błąd podczas aktualizacji statusu. Spróbuj ponownie później.');
-            });
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    return response.text();
+                })
+                .then(result => {
+                    console.log('Status updated:', result);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Wystąpił błąd podczas aktualizacji statusu. Spróbuj ponownie później.');
+                });
         };
-        
-      
-
-        
 
 
-       
 
-       // loadRandomLinkJs();
-      
-
+        ShowIntegrationTheCashRegisterSystem();
         ShowPhoneTopups();
-      
-
-
-
-
-        // updateTotalCostToOption2();
-   
+        setSideForProductType();
+        loadStatusOpenOffer();
 
         // handleSectionVisibility();
-        ShowIntegrationTheCashRegisterSystem();
+
+        // loadStatusOpenOffer();
+
 
 
 
